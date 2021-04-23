410650e04ccaeb4282bb305286f5956f581a3b48
Alina Minnibaeva <Minnibaeva.02@mail.ru>
2021-04-23 11:47:38 +0300

    add time line in daily schedule

--- ClientApp/src/components/DailySchedule.js
+++ ClientApp/src/components/DailySchedule.js
@@ -7,8 +7,17 @@ export class DailySchedule extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
 
+
+        var today = new Date(),
+            time =  today.toLocaleTimeString(),
+            day = today.getDay(),
+            month = today.getMonth();
+
+        this.state = {
+            time: time,
+            day: day,
+            month: month,
             classes: [
                 {
                     time: 'Время',
@@ -40,7 +49,64 @@ export class DailySchedule extends Component {
     }
 
 
+
+    currentDay() {
+        switch (this.state.day) {
+            case (1):
+                return 'понедельник';
+            case (2):
+                return 'вторник';
+            case (3):
+                return 'среда';
+            case (4):
+                return 'четверг';
+            case (5):
+                return ('пятница');
+            case (6):
+                return ('суббота');
+            case (7):
+                return ('воскресенье');
+            default:
+                return ('');
+        }
+    }
+
+
+
+    currentMonth() {
+        switch (this.state.month) {
+            case (0):
+                return 'января';
+            case (1):
+                return 'февраля';
+            case (2):
+                return 'марта';
+            case (3):
+                return 'апреля';
+            case (4):
+                return ('мая');
+            case (5):
+                return ('июня');
+            case (6):
+                return ('июля');
+            case (7):
+                return ('августа');
+            case (8):
+                return ('сентября');
+            case (9):
+                return ('октября');
+            case (10):
+                return ('ноября');
+            case (11):
+                return ('декабря');
+
+            default:
+                return ('');
+        }
+    }
+
     render() {
+
 
         function CurrentPair(index) {
             if (index + 2 === 2) {
@@ -62,6 +128,16 @@ export class DailySchedule extends Component {
 
         return (
             <div>
+                <div className="dailyScheduleMain">
+                    <div>
+                        <button className="dailyScheduleButton">←</button>
+                    </div>
+                    <div><p  className="dailyScheduleP">{this.state.day} {this.currentMonth()}, {this.currentDay()}</p>
+                    </div>
+                    <div>
+                        <button className="dailyScheduleButton">→</button>
+                    </div>
+                </div>
                 {content}
             </div>
             //сделать динамический вывод всех элементов массива через .map
