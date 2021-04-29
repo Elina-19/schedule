d4b3ab457f27e6b9714c5e0ad8c6545c84e8c91c
tououzxc <72223183+touou@users.noreply.github.com>
2021-04-29 10:30:23 +0300

    Update GoogleParser.cs

--- GoogleTableParser/GoogleParser.cs
+++ GoogleTableParser/GoogleParser.cs
@@ -1,14 +1,27 @@
-﻿using System;
+using System;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using System.IO;
 using System.Collections.Generic;
+using Google.Apis;
 
 namespace GoogleParser
 {
-    class Program
+    public class GoogleTableParser
     {
         // стоит сразу же выработать какой нибудь общий код стайл, которого вы все в команде будете придерживаться
+
+
+        // Убрать здесь main метод
+        // В проекте не может быть два main метода.
+        // И выглядит так, как будто вы пытались создать проект, но его создать не получилось и создали просто папку
+
+        // Во вторых добавьте в gitignore table_parser.json, потому что у вас там все credentials, что конечно же неправильно
+        // потому что дай волю, я могу их взять и просто задудосить и тогда гугл начнется ругаться, что за фигня, и заблочит ваше приложение
+
+        // И почитайте про асинхронные запросы. У вас сейчас все синхронно выполняется
+
+
         static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
 
         static readonly string AppName = "TableParserForProject";
@@ -19,20 +32,10 @@ namespace GoogleParser
 
         static SheetsService service;
 
-        // Убрать здесь main метод
-        // В проекте не может быть два main метода.
-        // И выглядит так, как будто вы пытались создать проект, но его создать не получилось и создали просто папку
-        
-        // Во вторых добавьте в gitignore table_parser.json, потому что у вас там все credentials, что конечно же неправильно
-        // потому что дай волю, я могу их взять и просто задудосить и тогда гугл начнется ругаться, что за фигня, и заблочит ваше приложение
-        
-        // И почитайте про асинхронные запросы. У вас сейчас все синхронно выполняется
-        static void Main(string[] args)
+        public static void TableAccess()
         {
-            
-
             GoogleCredential credential;
-            using (var stream =new FileStream("table_parser.json", FileMode.Open,FileAccess.Read))
+            using (var stream = new FileStream("table_parser.json", FileMode.Open, FileAccess.Read))
             {
                 credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
             }
@@ -42,19 +45,15 @@ namespace GoogleParser
                 HttpClientInitializer = credential,
                 ApplicationName = AppName,
             });
-
-            /// Main Array for Data
-            var mainArray=ReadEntries();
-            TableWriter(mainArray);
-
+            var arr = ReadEntries();
+            TableWriter(arr);
         }
 
-        // метод для вывода всей таблицы на консоль (позже допилить до распределителя данных) 
-        static void TableWriter(object[,] array)
+        private static void TableWriter(object[,] array)
         {
-            for (int i=0;i<43;i++)
+            for (int i = 0; i < 43; i++)
             {
-                for (int j=0;j<50;j++)
+                for (int j = 0; j < 50; j++)
                 {
                     Console.Write(array[i, j] + "|");
                     //
@@ -62,25 +61,24 @@ namespace GoogleParser
                 Console.WriteLine();
             }
         }
-        
-        static object[,] ReadEntries()
+
+        private static object[,] ReadEntries()
         {
-            
             var readRange = $"{sheetName}!B3:AY45";
             var request = service.Spreadsheets.Values.Get(SheetId, readRange);
 
             var response = request.Execute();
             var values = response.Values;
-            var list = new object[43,50];
+            var list = new object[43, 50];
             var j = 0;
             if (values != null && values.Count > 0)
             {
                 foreach (var item in values)
                 {
-                    for (int i=0;i<item.Count;i++)
+                    for (int i = 0; i < item.Count; i++)
                     {
-                        if (list[j, i] == "")
-                        { 
+                        if (item[i] =="")
+                        {
                             list[j, i] = "------";
                         }
                         else
@@ -95,10 +93,11 @@ namespace GoogleParser
                     j++;
                 }
                 return list;
-          
+
             }
             else
                 throw new Exception("NO DATA");
         }
     }
+    
 }
