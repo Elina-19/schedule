976c24e4272ca20eb3cb1fb239cf0d2288f42d00
HiiMark1 <65589740+HiiMark1@users.noreply.github.com>
2021-05-09 15:15:27 +0300

    Parse not merge cells & merge cells without commas

--- ExcelParser/Excel.cs
+++ ExcelParser/Excel.cs
@@ -11,7 +11,7 @@ namespace ExcelParser
     internal class Excel
     {
         private static int id = 0;
-        private readonly _Excel._Application excel = new _Excel.Application();
+        public readonly _Excel._Application excel = new _Excel.Application();
         private string path = "";
         private _Excel.Range Rng;
         private readonly _Excel.Workbook wb;
@@ -30,19 +30,24 @@ namespace ExcelParser
         {
             if (((_Excel.Range) ws.Cells[i, j]).Value2 != null)
                 return (string) ((_Excel.Range) ws.Cells[i, j]).Value2;
-            return "NULL.";
+            return " ";
         }
 
-        //Прочитать всю строку в столбце
-        public void parseRowToJson(int row, int column = 51)
+        //Прочитать всю строку в таблице
+        public void readRow(int row, int column = 51)
         {
             var timeForRow = readCell(row, 2);
 
             for (var i = 3; i <= column; i++)
             {
+                id++;
                 // Читаем строку в ячейке и проверяем находится ли она в объединение
                 var str = readCell(row, i);
-                if (!str.Equals("NULL."))
+                if (str.Equals(" "))
+                {
+                    continue;
+                }
+                else
                 {
                     Rng = (_Excel.Range) ws.Cells[row, i];
                     if ((bool) Rng.MergeCells)
@@ -57,62 +62,121 @@ namespace ExcelParser
                         else
                         {
                             //Если не содержит, то создаёт один объект урок и задает ему необходимые параметры
-                            var lesson = new Lesson();
-                            var strings = stringBetweenCapitalLetters(str);
-
-                            var groupsList = new List<string>();
-                            for (var a = i; a < i + mergeArea; a++) groupsList.Add(readCell(3, a));
-
-                            if (strings[0].Equals("Элективные курсы по физической культуре и спорту в "))
+                            if (str.Contains("Проектный практикум"))
                             {
-                                lesson.Discipline = "Элективные курсы по физической культуре и спорту";
-                                lesson.Teacher = "null";
-                                lesson.Groups = groupsList;
-                                var stringsForFizra = strings[2].Split(' ');
-                                lesson.Time = stringsForFizra[1];
-                                lesson.Id = ++id;
-                                for (var j = 0; j < lesson.Groups.Count(); j++)
-                                    Console.WriteLine(lesson.Groups.ToArray()[j]);
-                                Console.WriteLine(lesson);
+                                Console.WriteLine("Уходи");
+                                i = i + mergeArea - 1;
+                                id = id + mergeArea - 1;
+                                continue;
                             }
                             else
                             {
-                                lesson.Id = ++id;
-                                lesson.Time = timeForRow;
-                                lesson.Discipline = strings[0];
-                                lesson.Teacher = strings[1];
-                                lesson.Groups = groupsList;
+                                var lesson = new Lesson();
+                                string strBefore = "";
+                                bool isContain = false;
+                                if (str.Contains(':'))
+                                {
+                                    isContain = true;
+                                    string[] strs = str.Split(':');
+                                    strBefore = strs[0];
+                                    if (strs[1][0].Equals(' '))
+                                    {
+                                        str = strs[1].Substring(1);
+                                    }
+                                    else
+                                    {
+                                        str = strs[1];
+                                    }
+                                }
 
-                                writeToJsonFile(lesson);
+                                var strings = stringBetweenCapitalLetters(str);
 
-                                //Для проверки корректности работы
-                                //for (var j = 0; j < lesson.Groups.Count(); j++)
-                                //    Console.WriteLine(lesson.Groups.ToArray()[j]);
-                                //Console.WriteLine(lesson);
+                                var groupsList = new List<string>();
+                                for (var a = i; a < i + mergeArea; a++) groupsList.Add(readCell(3, a));
+                                if (strings[0].Equals("Элективные курсы по физической культуре и спорту в "))
+                                {
+                                    lesson.Discipline = "Элективные курсы по физической культуре и спорту";
+                                    lesson.Teacher = "null";
+                                    lesson.Groups = groupsList;
+                                    var stringsForFizra = strings[2].Split(' ');
+                                    lesson.Time = stringsForFizra[1];
+                                    lesson.Id = id;
+                                    for (var j = 0; j < lesson.Groups.Count(); j++)
+                                        Console.WriteLine(lesson.Groups.ToArray()[j]);
+                                    Console.WriteLine(lesson);
+                                }
+                                else
+                                {
+                                    lesson.Id = id;
+                                    lesson.Time = timeForRow;
+                                    if (isContain)
+                                    {
+                                        lesson.Discipline = strBefore + " " + strings[0];
+                                    }
+                                    else
+                                    {
+                                        lesson.Discipline = strings[0];
+                                    }
+
+                                    lesson.Teacher = strings[1];
+                                    lesson.Groups = groupsList;
+
+                                    //Для проверки корректности работы
+                                    for (var j = 0; j < lesson.Groups.Count(); j++)
+                                        Console.WriteLine(lesson.Groups.ToArray()[j]);
+                                    Console.WriteLine(lesson);
+                                }
                             }
                         }
 
+
                         //Пропускает следующие ячейки строки
                         i = i + mergeArea - 1;
+                        id = id + mergeArea - 1;
                     }
                     //Если ячейка не объединенная
                     else
                     {
+                        string strBefore = "";
+                        bool isContain = false;
+                        if (str.Contains(':'))
+                        {
+                            isContain = true;
+                            string[] strs = str.Split(':');
+                            strBefore = strs[0];
+                            if (strs[1][0].Equals(' '))
+                            {
+                                str = strs[1].Substring(1);
+                            }
+                            else
+                            {
+                                str = strs[1];
+                            }
+                        }
+
                         var lesson = new Lesson();
                         var strings = stringBetweenCapitalLetters(str);
                         var groupsList = new List<string>();
                         groupsList.Add(readCell(3, i));
 
-                        lesson.Id = ++id;
+                        lesson.Id = id;
                         lesson.Time = timeForRow;
-                        lesson.Discipline = strings[0];
+                        if (isContain)
+                        {
+                            lesson.Discipline = strBefore + " " + strings[0];
+                        }
+                        else
+                        {
+                            lesson.Discipline = strings[0];
+                        }
+
                         lesson.Teacher = strings[1];
                         lesson.Groups = groupsList;
 
                         //Для проверки корректности работы
-                        //for (var j = 0; j < lesson.Groups.Count(); j++)
-                        //    Console.WriteLine(lesson.Groups.ToArray()[j]);
-                        //Console.WriteLine(lesson);
+                        for (var j = 0; j < lesson.Groups.Count(); j++)
+                            Console.WriteLine(lesson.Groups.ToArray()[j]);
+                        Console.WriteLine(lesson);
                     }
                 }
             }
@@ -121,97 +185,97 @@ namespace ExcelParser
         //Получение 3 строк: Названия предмета, ФИО учителя, Остальное
         public string[] stringBetweenCapitalLetters(string str)
         {
+            //Убрать текст между скобками
             str = Regex.Replace(str, @"\(.*?\)", "");
             List<int> ints = new List<int>();
+            bool isHavePatronymic = false;
+            bool isHaveName = false;
 
             for (var i = 1; i < str.Length; i++)
+            {
                 if (char.IsUpper(str[i]))
                 {
-                    if (ints.Count < 2)
+                    if (ints.Count < 1)
                     {
                         ints.Add(i);
                     }
                     else
                     {
                         ints.Add(i);
-                        for (var j = i; j < str.Length; j++)
-                            if (str[j].Equals('.'))
+                        isHaveName = true;
+                        if (str[i + 1].Equals('.'))
+                        {
+                            if (char.IsUpper(str[i + 2]))
                             {
-                                ints.Add(j + 1);
-                                break;
+                                ints.Add(i + 2);
+                                isHavePatronymic = true;
+                                ints.Add(i + 4);
                             }
                             else
                             {
-                                if (str[j].Equals(' '))
-                                {
-                                    ints.Add(j + 1);
-                                    break;
-                                }
+                                ints.Add(i + 1);
+                                break;
                             }
+                        }
+                        else
+                        {
+                            isHavePatronymic = true;
+                            for (var j = i; j < str.Length; j++)
+                                if (char.IsUpper(str[j]) && ints.Count < 3)
+                                {
+                                    ints.Add(j);
+                                    for (int k = j; k < str.Length; k++)
+                                    {
+                                        if (str[k].Equals(' '))
+                                        {
+                                            ints.Add(k);
+                                        }
+                                    }
+                                }
 
-                        break;
+                            break;
+                        }
                     }
                 }
 
-            var strings = new string[3];
-
-            strings[0] = str.Substring(0, ints[0]);
-            strings[1] = str.Substring(ints[0], ints[3] - ints[0]);
-            strings[2] = str.Substring(ints[3]);
-            strings[2] = findId(strings[2]);
-
-            return strings;
-        }
-
-        //Ищет в строке 3 или 4 значное число
-        public string findId(string str)
-        {
-            str = str + " ";
-            for (int i = 0; i < str.Length - 3; i++)
-            {
-                char c1 = str[i];
-                if (Char.IsDigit(c1))
+                if (ints.Count == 1 && str[i].Equals('.'))
                 {
-                    int j = i;
-                    for (; j < i + 4; j++)
-                    {
-                        char c2 = str[j];
-                        if (!Char.IsDigit(c2))
-                        {
-                            break;
-                        }
-                    }
+                    ints.Add(i - 1);
+                    break;
+                }
 
-                    if (j == i + 3)
-                    {
-                        str = str.Substring(i, 4);
-                        break;
-                    }
-
-                    j = i;
-                    for (; j < i + 3; j++)
-                    {
-                        char c2 = str[j];
-                        if (!Char.IsDigit(c2))
-                        {
-                            break;
-                        }
-                    }
-
-                    if (j == i + 2)
-                    {
-                        str = str.Substring(i, 3);
-                        break;
-                    }
+                if (ints.Count == 1 && char.IsDigit(str[i]))
+                {
+                    ints.Add(i - 1);
+                    break;
                 }
             }
 
-            return str;
-        }
+            var strings = new string[3];
 
-        public void writeToJsonFile(Lesson lesson)
-        {
-            //TODO запись на json
+            if (isHaveName)
+            {
+                if (isHavePatronymic)
+                {
+                    strings[0] = str.Substring(0, ints[0]);
+                    strings[1] = str.Substring(ints[0], ints[3] - ints[0]);
+                    strings[2] = str.Substring(ints[3]);
+                }
+                else
+                {
+                    strings[0] = str.Substring(0, ints[0]);
+                    strings[1] = str.Substring(ints[0], ints[2] - ints[0] + 1);
+                    strings[2] = str.Substring(ints[2]);
+                }
+            }
+            else
+            {
+                strings[0] = str.Substring(0, ints[0]);
+                strings[1] = str.Substring(ints[0], ints[1] - ints[0]);
+                strings[2] = str.Substring(ints[1]);
+            }
+
+            return strings;
         }
     }
 }
